e1fb394bce54e90907ab73c4230845a48c10d81e
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2015-11-19 20:45:36 +0500

    bug report

---
+++ RazorGeneratorTest/ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using RazorGeneratorTest.Views.Test;
+
+namespace ConsoleApp
+{
+	static class Program
+	{
+		static void Main()
+		{
+			PrintView();
+			PrintHelper();
+		}
+
+		private static void PrintHtml(WebViewPage<string> view, string color)
+		{
+			try
+			{
+				view.ViewData.Model = color;
+				view.PushContext(new WebPageContext(), new StringWriter());
+				view.Execute();
+				Console.Out.WriteLine(view.Output.ToString());
+				view.PopContext();
+			}
+			catch (Exception ex)
+			{
+				Console.Out.WriteLine(ex.ToString());
+			}
+		}
+
+		private static void PrintView()
+		{
+			PrintHtml(new Standard(), "green");
+		}
+
+		private static void PrintHelper()
+		{
+			PrintHtml(new Helper(), "red");
+		}
+	}
+}
---
+++ RazorGeneratorTest/RazorGeneratorTest/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using RazorGeneratorTest.Views.Test;
+
+namespace RazorGeneratorTest.Controllers
+{
+	public class TestController : Controller
+	{
+		// GET: Test
+		public ActionResult Standard()
+		{
+			return View(model: "green");
+		}
+
+		public ActionResult MvcView()
+		{
+			var view = new Standard {ViewData = {Model = "red"}};
+			view.PushContext(new WebPageContext(), new StringWriter());
+			view.Execute();
+			var result = view.Output.ToString();
+			view.PopContext();
+			return Content(result);
+		}
+
+		public ActionResult Helper()
+		{
+			return View(model: "green");
+		}
+	}
+}
